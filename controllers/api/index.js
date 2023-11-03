f8d5bb8f24da019ffe0b4a34f14119b290414d5a
AllieStewart <megaxis1217@gmail.com>
2023-11-03 14:14:21 -0500

    fixed index.js in controllers/api

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,6 @@ const tvShowRoutes = require('./tvShowRoutes');
 const genreRoutes = require('./genreRoutes');
 const userRoutes = require('./userRoutes');
 
-router.use('/users', userRoutes);
-
 router.use('/tvshow', tvShowRoutes);
 router.use('/genre', genreRoutes);
 router.use('/user', userRoutes);

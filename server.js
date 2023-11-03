cc26437107c95865a8624242f0c5302b5df0b962
Acolyte03 <Acolyte03@gmail.com>
2023-11-03 13:48:20 -0500

    Added a very baseline header, installed our node package and have it correctly imported into the handlebars files.

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
+const sequelize = require('../My-Queue/node_modules/sequelize/lib/dialects/mysql/connection-manager');
 // const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});

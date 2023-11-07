e69086492cb817b8c90092ede94df9806684bc22
AllieStewart <megaxis1217@gmail.com>
2023-11-07 13:50:00 -0600

    added more data in /seeds

--- seeds/reviewSeeds.js
+++ seeds/reviewSeeds.js
@@ -4,8 +4,20 @@ const reviewData = [
     {
         id: 1,
         comment: "This show is bad and you should feel bad.",
+        user_id: 1,
+        tv_show_id: 1
+    },
+    {
+        id: 2,
+        comment: "It's okay, I guess.",
         user_id: 2,
         tv_show_id: 2
+    },
+    {
+        id: 3,
+        comment: "OMG SO GOOD LALAFLKAJGALAG;WE",
+        user_id: 3,
+        tv_show_id: 3
     },
 ];
 

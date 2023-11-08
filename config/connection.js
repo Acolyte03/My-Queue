538faa8dcbd3d763fd9e5316e403b3e9e52df446
AllieStewart <144143332+AllieStewart@users.noreply.github.com>
2023-11-08 15:12:32 -0600

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,14 @@
+// Start of JS file
+// Connection using Sequelize.
 const Sequelize = require('sequelize');
 require('dotenv').config();
-// require('dotenv').config({ path:'../.env'})
-// console.log(require("dotenv").config());
-// const path = require('path');
-// require('dotenv').config({
-//   path: path.resolve('config.env'),
-// });
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
-   
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -25,3 +21,4 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
+// End of JS file
